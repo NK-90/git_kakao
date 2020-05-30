e088f1a2740da2723baa508fbdf92c72131cd31c
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-30 22:28:46 +0900

    kakao_login ver2

--- kakao_login.py
+++ kakao_login.py
@@ -10,6 +10,6 @@ app.start('c:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe')
 
 
 #잠시대기후 비번입력
-time.sleep(2)
-# pywinauto.mouse.click(button='left', coords=(1121,795))
+time.sleep(1.7)
+pywinauto.mouse.click(button='left', coords=(1121,795))
 send_keys('Wlqwndfur423!{ENTER}')
