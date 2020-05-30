471ece1ec0dc5a937d4189e56d0cba654cece824
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-31 07:41:31 +0900

    True_kakao ver2(pass)

--- 프로젝트/True_kakao.py
+++ 프로젝트/True_kakao.py
@@ -1,29 +1,22 @@
 import pywinauto
 from pywinauto.keyboard import send_keys
-pwa_app = pywinauto.application.Application()
-
-
-pwa_app.start('c:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe')
+app = pywinauto.application.Application()
 
 
 
-
-
-for i in range(100000):
-    try:
-
-        w_handle = pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0]
-        window = pwa_app.window(handle=w_handle)
-        window.click_input()
-        send_keys('Wlqwndfur423!{ENTER}')
-
-    except:
-        pass
+#앱을 시작한다
+app.start('c:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe')
 
 
 
+#앱이 나올때까지 대기한 후, 나오면 윈도우 인자값(=val)을 받아온다
+val = pywinauto.timings.wait_until_passes(20, 0.5, lambda: pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0])
+window = app.window(handle=val)
 
-# w_handle = pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0]
-# window = pwa_app.window(handle=w_handle)
-# window.click_input()
-# send_keys('Wlqwndfur423!{ENTER}')
+
+
+#그 윈도우를 클릭 후, 비번을 입력한다
+window.click_input()
+send_keys('Wlqwndfur423!{ENTER}')
+
+
