08b34b0070846661f8991093266a3722599e5697
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-05-31 08:05:07 +0900

    add wait_kakao.exe

--- 프로젝트/wait_kakao.py
+++ 프로젝트/wait_kakao.py
@@ -10,8 +10,11 @@ app.start('c:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe')
 
 
 #앱이 나올때까지 대기한 후, 나오면 윈도우 인자값(=val)을 받아온다
-val = pywinauto.timings.wait_until_passes(20, 0.5, lambda: pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0])
-window = app.window(handle=val)
+try:
+    val = pywinauto.timings.wait_until_passes(20, 0.5, lambda: pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0])
+    window = app.window(handle=val)
+except:
+    pass
 
 
 
