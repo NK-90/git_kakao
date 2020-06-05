e6b2523831a0b7d3bfad1708a8543876b6c0a0c1
NK-90 <62362977+NK-90@users.noreply.github.com>
2020-06-05 20:44:28 +0900

    add Run_kakao(pass)

--- 프로젝트/Run_kakao.py
+++ 프로젝트/Run_kakao.py
@@ -9,9 +9,9 @@ app.start('c:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe')
 
 
 
-#앱이 나올때까지 대기한 후, 나오면 윈도우 인자값(=val)을 받아온다  / #비번을 입력한다
+#앱이 나올때까지 대기한 후, 나오면 윈도우 인자값(=val)을 받아온다  / #클릭 하고선, 비번을 입력한다
 try:
-    val = pywinauto.timings.wait_until_passes(40, 0.5, lambda: pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window')[0])
+    val = pywinauto.timings.wait_until_passes(20, 0.5, lambda: pywinauto.findwindows.find_windows(title=u'\uce74\uce74\uc624\ud1a1', class_name='EVA_Window_Dblclk')[0])
     window = app.window(handle=val)
     window.click_input()
     send_keys('Wlqwndfur423!{ENTER}')
